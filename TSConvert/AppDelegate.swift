5e7717c188158a1ea62571834db90904c4a7f4f2
Benjamin Yunker <bwyunker@benjamins-mbp.home>
2017-02-12 16:54:05 -0500

    Initial Commit

---
+++ TSConvert/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TSConvert
+//
+//  Created by Benjamin Yunker on 2/12/17.
+//  Copyright © 2017 Benjamin Yunker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
